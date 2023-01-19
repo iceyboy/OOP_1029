d9c9c5e0f74d053351f6f56e952927bc84d20301
Ryan King <ryan.king2016@loretomilford.net>
2023-01-19 15:27:35 +0000

    Add files via upload

---
+++ HelloWorldMethods.java
@@ -0,0 +1,24 @@
+
+public class HelloWorldMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		goodMorning();
+		goodEvening();
+		goodNight();
+	}
+
+	public static void goodMorning() {
+		System.out.println("Good Morning!");
+	}
+
+	public static void goodEvening() {
+		System.out.println("Good Evening!");
+		goodNight();
+	}
+
+	public static void goodNight() {
+		System.out.println("Good Night!");
+	}
+
+}
