2266fe0aca46ea69887af1fe60137accc0ad9093
Ryan King <ryan.king2016@loretomilford.net>
2023-01-20 00:24:35 +0000

    Add files via upload

---
+++ T5.java
@@ -0,0 +1,36 @@
+
+public class T5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+bigM();
+bigO();
+bigO();
+bigO();
+bigO();
+	}
+	
+	public static void bigM() {
+		
+		System.out.println("M       M");
+		System.out.println("MM     MM");
+		System.out.println("M M   M M");
+		System.out.println("M  M M  M");
+		System.out.println("M   M   M");
+		System.out.println("\n");
+		
+	}
+	
+	public static void bigO() {
+		
+		System.out.println("  O O O  ");
+		System.out.println("O       O");
+		System.out.println("O       O");
+		System.out.println("O       O");
+		System.out.println("  O O O  ");
+		System.out.println("\n");
+		
+		
+	}
+
+}
