ce9118fedfe113a538d45141afe0a97388a553c1
Ryan King <ryan.king2016@loretomilford.net>
2023-01-21 15:11:47 +0000

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,41 @@
+package week2;
+
+public class Car {
+	private String make;
+	private String model;
+	private String colour;
+	private double engineSize;
+
+	public Car(String make, String model, String colour, double eSize) {
+		this.make = make;
+		this.model = model;
+		this.colour = colour;
+		this.engineSize = eSize;
+	}
+
+	public void setColour(String newCol) {
+		if (newCol != null) {
+			this.colour = newCol;
+		}
+	}
+
+	public String getColour() {
+		return this.colour;
+	}
+
+	public void drive() {
+		System.out.println("I am driving");
+	}
+
+	public void start() {
+		System.out.println("I am starting");
+	}
+
+	public void stop() {
+		System.out.println("I am stopping");
+	}
+
+	public void changeGear(int gear) {
+		System.out.println("Gear changed to " + gear);
+	}
+}
