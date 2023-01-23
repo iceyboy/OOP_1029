56f3aec8b8474c7d6617040c7e88e5aebf68d2c4
Ryan King <ryan.king2016@loretomilford.net>
2023-01-23 01:02:17 +0000

    Add files via upload

---
+++ aoifeTester.java
@@ -0,0 +1,21 @@
+package aoifeAI;
+
+public class aoifeTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		aoife myGF = new aoife("ballyage", "happy", "wrapped up", 6);
+		
+		myGF.setOutfit(null);
+		
+		
+		myGF.hair();
+		myGF.mood();
+		myGF.hungry();
+		myGF.walk();
+		myGF.getOutfit();
+
+	}
+
+}
