f0e64a2458d7ebf7c8adbc6cff9f1f3a84a5bdc3
Ryan King <ryan.king2016@loretomilford.net>
2023-01-24 22:24:45 +0000

    Add files via upload

---
+++ StudentTester.java
@@ -0,0 +1,22 @@
+package week31029;
+
+public class StudentTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student s1 = new Student(1234, "Homer", "CSC1029", 'S');
+		
+	//	Student s1 = null;
+		
+	//	System.out.println("ID: " +s1.getStudentId());
+	//	System.out.println("Name: " + s1.getName());
+	//	System.out.println("Course: " + s1.getCourse());
+	//	System.out.println("Faculty: " + s1.getFaculty());
+		
+		System.out.println(s1);
+		
+		
+	}
+
+}
