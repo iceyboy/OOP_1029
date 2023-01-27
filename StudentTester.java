163be128387ff2f7f1f52beed80bd2a22dd4c7b9
Ryan King <ryan.king2016@loretomilford.net>
2023-01-27 23:52:46 +0000

    Add files via upload

--- StudentTester.java
+++ StudentTester.java
@@ -1,22 +1,21 @@
-package week31029;
+package demol9;
 
 public class StudentTester {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// A class is a 'blueprint' for creating objects
+		// An object is an instance of a class
+		// s1 & s2 are references to Student object instances
+		// We can use s1 & s2 to access 'public' methods (and data)
+		// However, all data within an object should be 'private' by default
+		// You then use 'public' setters/getters or other methods to access.
+		
+		Student s1 = new Student(1, "Homer", "Computer Science", 'S');
+		Student s2 = new Student(2, "Bart", "Software Engineering", 'S');
+		
+		System.out.println(s1); // will call 'toString' for s1 (if it has been defined)
+		System.out.println(s2); // each object will have its own copy of instance data
 
-		Student s1 = new Student(1234, "Homer", "CSC1029", 'S');
-		
-	//	Student s1 = null;
-		
-	//	System.out.println("ID: " +s1.getStudentId());
-	//	System.out.println("Name: " + s1.getName());
-	//	System.out.println("Course: " + s1.getCourse());
-	//	System.out.println("Faculty: " + s1.getFaculty());
-		
-		System.out.println(s1);
-		
-		
 	}
 
 }
