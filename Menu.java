0e2ca3780b79a895970e6c678265206d630303ba
Ryan King <ryan.king2016@loretomilford.net>
2023-02-01 23:28:27 +0000

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -1,35 +1,91 @@
-package week31029;
+package practical3;
 
 import java.util.Scanner;
 
 public class Menu {
-	private String items[];
-	private String title;
-	private Scanner input;
-
-	public Menu(String title, String data[]) {
+	
+	private String options[];	
+	
+	private String title;	
+	
+	private Scanner input;	
+	
+	public Menu(String title, String options[]) {
+		
 		this.title = title;
-		this.items = data;
-		this.input = new Scanner(System.in);
+		
+		copyOptions(options);
+		
+		input = new Scanner(System.in);
+		
 	}
-
-	public int getUserChoice() {
+	
+	public int getChoice() {
+		
 		display();
-		System.out.print("Enter Selection: ");
-		int value = input.nextInt();
-		return value;
+		
+		System.out.print("Enter choice: ");
+		
+		int choice = input.nextInt();
+		
+		return choice;
+		
 	}
+	
+	public void display() {
+		
+		if (title != null && options !=null) {
 
-	private void display() {
-		System.out.println(title);
-		for (int i = 0; i < title.length(); i++) {
-			System.out.print("+");
+			System.out.println(title);
+			
+			for(int i=0;i<title.length();i++) {
+				
+				System.out.print("+");
+				
+			}
+			
+			System.out.println("\n");
+			
+			int count = 1;
+			
+			for(String str : options) {
+				
+				System.out.println(count+". "+str);
+				
+				count++;
+				
+			}
+			
+			System.out.println();
+			
 		}
-		System.out.println();
-		for (int opt = 1; opt <= items.length; opt++) {
-			System.out.println(opt + ". " + items[opt - 1]);
+		
+		else {
+			
+			System.out.println("Menu not defined.");
+			
 		}
-		System.out.println();
+		
 	}
-
+	
+	public void copyOptions(String data[]) {
+		
+		if ( data != null) {
+			
+			options = new String[data.length];
+			
+			for(int index=0;index<data.length;index++) {
+				
+				options[index] = data[index];
+				
+			}
+			
+		}else {
+			
+			options = null;
+			
+		}
+		
+	}
+	
 }
